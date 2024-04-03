1089aa3160bae48ee94e68212c1558a8fc78d3f5
Silviu Stăncioiu <silviu.stancioiu@gmail.com>
2024-04-03 21:24:37 +0300

    forgor to save scene

--- Assets/Scripts/CircularTeleport.cs
+++ Assets/Scripts/CircularTeleport.cs
@@ -1,41 +1,87 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CircularTeleport : MonoBehaviour
 {
-    [SerializeField]
-    private bool      _useGravity        = true;
-    [SerializeField]                     
-    private Transform _rightHand         = default;
-    [SerializeField]                     
-    private float     _teleportDistance  = 10.0f;
-    [SerializeField]                     
-    private float     _arcDuration       = 3.0f;
-    [SerializeField]                     
-    private float     _segmentsCount     = 60;
-    [SerializeField]
-    private LayerMask _teleportLayerMask = -1;
+    private const float          FLOOR_BIAS             = 0.01f;
+                                                        
+    [SerializeField]                                    
+    private       bool           _useGravity            = true;
+    [SerializeField]                                    
+    private       Transform      _rightHand             = default;
+    [SerializeField]                                    
+    private       float          _teleportDistance      = 10.0f;
+    [SerializeField]                                    
+    private       float          _arcDuration           = 3.0f;
+    [SerializeField]                                    
+    private       int            _segmentsCount         = 60;
+    [SerializeField]                                    
+    private       LayerMask      _teleportLayerMask     = -1;
+    [SerializeField]             
+    private       Transform      _targetIndicatorPrefab = default;
+    [SerializeField]             
+    private       Material       _arcMaterial;
+                                 
+    private       float          _scale                 = 1.0f;
+    private       Transform      _targetIndicator;
+    private       LineRenderer[] _lineRenderers         = null;
 
-    private float     _scale             = 1.0f;
+    private void Awake()
+    {
+        _targetIndicator = Instantiate(_targetIndicatorPrefab);
+        _targetIndicator.gameObject.SetActive(false);
+
+        _lineRenderers = new LineRenderer[_segmentsCount];
+        for (int i = 0; i < _segmentsCount; i++)
+        {
+            var lineObject = new GameObject("LineRenderer_" + i);
+            _lineRenderers[i] = lineObject.AddComponent<LineRenderer>();
+            _lineRenderers[i].positionCount = 2;
+            _lineRenderers[i].startWidth = _lineRenderers[i].endWidth = 0.1f; // TODO: Hard-coded constant
+            _lineRenderers[i].material = _arcMaterial;
+
+            lineObject.transform.SetParent(transform);
+            lineObject.transform.localPosition = Vector3.zero;
+            lineObject.transform.localRotation = Quaternion.identity;
+            lineObject.transform.localScale    = Vector3.one;
+        }
+    }
 
     private void Update()
     {
         _scale = transform.lossyScale.x;
+
+        DrawArc();
     }
 
-    private void OnDrawGizmos()
+    private void DrawArc()
     {
-        if (_rightHand == null)
-            return;
+        var arcTime = FindProjectileCollision(out var hit);
+        if (arcTime == float.MaxValue)
+        {
+            _targetIndicator.gameObject.SetActive(false);
+            foreach (var lineRenderer in _lineRenderers)
+                lineRenderer.gameObject.SetActive(false);
+        }
+        else
+        {
+            _targetIndicator.gameObject.SetActive(true);
+            foreach (var lineRenderer in _lineRenderers)
+                lineRenderer.gameObject.SetActive(true);
 
-        var projectileTime = FindProjectileCollision(out var hit);
-        if (projectileTime == float.MaxValue)
-            return;
+            _targetIndicator.position = GetArcPositionAtTime(arcTime) + Vector3.up * FLOOR_BIAS;
 
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(GetArcPositionAtTime(projectileTime), 1.0f);
+            for (var i = 0; i < _segmentsCount; i++)
+            {
+                var t = (float)i / _segmentsCount;
+                var nextT = ((float)(i + 1)) / _segmentsCount;
+                var time = Mathf.Lerp(0.1f, arcTime, t); // TODO: Hard-coded constant for the start time
+                var nextTime = Mathf.Lerp(0.1f, arcTime, nextT); // TODO: Hard-coded constant for start time
+                _lineRenderers[i].SetPosition(0, GetArcPositionAtTime(time));
+                _lineRenderers[i].SetPosition(1, GetArcPositionAtTime(nextTime));
+            }
+        }
     }
-
+    
     private float FindProjectileCollision(out RaycastHit hitInfo)
     {
         var timeStep = _arcDuration / _segmentsCount;
